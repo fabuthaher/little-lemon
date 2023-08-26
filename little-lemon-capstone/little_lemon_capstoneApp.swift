2c709674b48f4287f776934e2589e4eee455b623
Farah Abuthaher <farah.abuthaher@pwc.com>
2023-08-26 20:59:00 +0300

    Initial Commit

---
+++ little-lemon-capstone/little_lemon_capstoneApp.swift
@@ -0,0 +1,20 @@
+//
+//  little_lemon_capstoneApp.swift
+//  little-lemon-capstone
+//
+//  Created by pwc on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct little_lemon_capstoneApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
